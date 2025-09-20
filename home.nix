3e964f0ea5184ca08fe6d9f47c6e7e80938a0d0c
anttiharju <antti@harju.io>
2025-09-20 16:59:21 +0300

    Add aerospace to home-manager

--- home.nix
+++ home.nix
@@ -3,9 +3,17 @@
     username = "antti";
     homeDirectory = "/Users/antti";
     stateVersion = "25.05";
-    file.".hushlogin" = {
-      source = ./.hushlogin;
+    file = {
+      ".hushlogin" = {
+        source = ./.hushlogin;
+      };
+      ".aerospace.toml" = {
+        source = ./.aerospace.toml;
+      };
     };
+    packages = [
+      pkgs.aerospace
+    ];
   };
   programs = {
     home-manager.enable = true;
