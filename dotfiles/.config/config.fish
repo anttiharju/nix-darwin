d21c9b2fda5976951946667a1333f8e239226031
anttiharju <antti@harju.io>
2025-09-20 18:23:15 +0300

    Load keychain automatically

--- dotfiles/.config/config.fish
+++ dotfiles/.config/config.fish
@@ -1,6 +1,11 @@
-# Disable greeting
+# This is slow, hence the if condition.
+if not ssh-add -l >/dev/null 2>&1
+  ssh-add --apple-load-keychain 2> /dev/null
+end
+
+# Disable greeting.
 set fish_greeting
 
-# Abbreviations 
+# Abbreviations.
 abbr -a "s" "sudo"
 abbr -a "ds" "sudo darwin-rebuild switch"
