aeef81e3cd45562455e89933bf18e6daec95c538
anttiharju <antti@harju.io>
2025-09-29 13:41:14 +0300

    Scaffolding

--- utils/gprc.sh
+++ utils/gprc.sh
@@ -3,10 +3,15 @@ set -euo pipefail
 
 # Git PR Chrome:
 # Use chrome-cli to easily access GitHub PRs in Google Chrome without polluting tabs
-# $ chrome-cli list links
-# [1641357594] https://calendar.google.com/calendar/u/0/r
-# [1641357598] https://smartlyio.atlassian.net/jira/software/c/projects/DOPS/boards/580?assignee=712020%3A98d9cca5-ecc5-4aa3-b20b-1df77656ed34&isEligibleForUserSurvey=true
-# [1641357572] https://github.com/smartlyio/typescript-nodejs-smartly-starter
-# [1641357579] https://github.com/anttiharju/nix-darwin
 
-chrome-cli list links
+ids=()
+urls=()
+while read -r line; do
+    id=$(echo "$line" | awk -F'[][]' '{print $2}')
+    url=$(echo "$line" | awk '{print $2}')
+    ids+=("$id")
+    urls+=("$url")
+done < <(chrome-cli list links | grep 'github.com')
+
+echo "${ids[@]}"
+echo "${urls[@]}"
