fd749d0e97d3e2377f2846250fd4c7512c2b2dca
anttiharju <antti@harju.io>
2025-10-25 23:13:12 +0200

    Remove vim-wrapper

--- utils/vim-wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Check if this is likely a commit message (temporary file in .git or contains COMMIT_EDITMSG)
-if [[ "$1" == *"COMMIT_EDITMSG"* ]] || [[ "$1" == *".git/"*"COMMIT_EDITMSG" ]]; then
-    vim +startinsert "$@"
-else
-    vim "$@"
-fi
