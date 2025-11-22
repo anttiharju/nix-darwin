86808108d09b2502255f9c78de3c1207523c1115
anttiharju <antti@harju.io>
2025-11-22 12:36:45 +0100

    Allow nix-community cachix in project-specific flakes

--- flake.nix
+++ flake.nix
@@ -42,6 +42,12 @@
             )
           ];
           nix.settings.experimental-features = "nix-command flakes";
+          nix.settings.trusted-substituters = [
+            "https://nix-community.cachix.org"
+          ];
+          nix.settings.trusted-public-keys = [
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          ];
 
           # Set Git commit hash for darwin-version.
           system.configurationRevision = self.rev or self.dirtyRev or null;
